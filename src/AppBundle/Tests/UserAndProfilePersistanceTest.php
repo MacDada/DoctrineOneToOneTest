224c16b5c27cd22331b8828c17b736b733d47000
Dawid Nowak <code@dnowak.pl>
2015-05-22 23:26:32 +0200

    whitespace

--- src/AppBundle/Tests/UserAndProfilePersistanceTest.php
+++ src/AppBundle/Tests/UserAndProfilePersistanceTest.php
@@ -7,51 +7,51 @@ use AppBundle\Entity\User;
 
 class UserAndProfilePersistanceTest extends KernelTestCase
 {
-	public function testProfileIsSavedWithUser()
-	{
-		static::bootKernel();
+    public function testProfileIsSavedWithUser()
+    {
+        static::bootKernel();
 
-		$objectManager = $this->getObjectManager();
+        $objectManager = $this->getObjectManager();
         $username = 'test'.time();
 
-		$user = new User($username);
+        $user = new User($username);
         $objectManager->persist($user);
-		$objectManager->flush();
+        $objectManager->flush();
 
-		$user->createProfile();
-		$objectManager->flush();
+        $user->createProfile();
+        $objectManager->flush();
 
-		$this->assertUserHasProfile($user, $username);
+        $this->assertUserHasProfile($user, $username);
 
         $foundUser = $this->findUser($username);
-		$this->assertUserHasProfile($foundUser, $username);
-	}
+        $this->assertUserHasProfile($foundUser, $username);
+    }
 
-	private function assertUserHasProfile(User $user, $expectedUsername)
-	{
-		$this->assertSame($expectedUsername, $user->getUsername());
+    private function assertUserHasProfile(User $user, $expectedUsername)
+    {
+        $this->assertSame($expectedUsername, $user->getUsername());
 
         $profile = $user->getProfile();
 
-		$this->assertInstanceOf('AppBundle\Entity\UserProfile', $profile);
-		$this->assertSame($user->getId(), $profile->getId());
+        $this->assertInstanceOf('AppBundle\Entity\UserProfile', $profile);
+        $this->assertSame($user->getId(), $profile->getId());
         $this->assertSame($user->getUsername(), $profile->getUsername());
-	}
+    }
 
-	private function findUser($username)
-	{
-		return $this->getObjectManager()
-			->getRepository('AppBundle:User')
-			->findOneByUsername($username);
-	}
+    private function findUser($username)
+    {
+        return $this->getObjectManager()
+            ->getRepository('AppBundle:User')
+            ->findOneByUsername($username);
+    }
 
-	/**
-	 * @return \Doctrine\Common\Persistence\ObjectManager
-	 */
-	private function getObjectManager()
-	{
-		return static::$kernel->getContainer()
-			->get('doctrine')
-			->getManager();
-	}
+    /**
+     * @return \Doctrine\Common\Persistence\ObjectManager
+     */
+    private function getObjectManager()
+    {
+        return static::$kernel->getContainer()
+            ->get('doctrine')
+            ->getManager();
+    }
 }
